12f169f27ffd7bc58dba50ee6868827f7bbee171
Sam <samarony.barros@gmail.com>
2024-02-11 16:58:10 +0100

    automatic setup

--- samaronybarrosblog/config/_gen.toml.sh
+++ samaronybarrosblog/config/_gen.toml.sh
@@ -1,15 +1,56 @@
 #!/bin/bash
 
-# Check if at least one argument is provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <file1.toml> [<file2.toml> ...]"
-    exit 1
-fi
+SHARED_JSON="_shared.json"
+HUGO_FILE="../hugo.toml"
 
-# Specify the output file name
-output_file="../hugo.toml"
+# Function to create backup files for each .toml file
+create_bkp_files() {
+    mkdir -p tmp
+    cp *.toml tmp
+    cp *.json tmp
+    cd tmp
+}
 
-# Use cat to concatenate all provided .toml files and redirect the output to the specified file
-cat "$@" > "$output_file"
+# Function to read shared information from SHARED_JSON and store in a file
+read_shared_info() {
+    jq -r 'to_entries[] | .key + "=" + .value' "$SHARED_JSON" > shared_info.txt
+}
 
-echo "Combined .toml files into $output_file"
+delete_shared_info() {
+    rm -f shared_info.txt
+    rm -rf tmp
+}
+
+# Function to replace values in each .toml file
+replace_values() {
+    local file="$1"
+    local shared_info_file="shared_info.txt"
+    while IFS="=" read -r key value; do
+        sed -i.bak "s/\[$key\]/$value/g" "$file"
+    done < "$shared_info_file"
+}
+
+merge_toml_files() {
+    cd ..
+    cat tmp/*.toml > lang.toml
+}
+
+
+# Main function
+main() {
+    cd languages
+    create_bkp_files
+    read_shared_info
+    for file in *.toml; do
+        replace_values "$file"
+    done
+    merge_toml_files
+    delete_shared_info
+    cd ..
+    cat main.toml languages/lang.toml social.toml > $HUGO_FILE
+    rm languages/lang.toml
+}
+
+main
+
+echo "Combined .toml files into $HUGO_FILE"
